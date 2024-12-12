c2dd76a501f038bb654e05325af2798156c86c6b
RusticDeveloper <72155045+RusticDeveloper@users.noreply.github.com>
2024-12-11 23:16:09 -0500

    Update sesor_info_generator.sh

--- sesor_info_generator.sh
+++ sesor_info_generator.sh
@@ -12,7 +12,10 @@ while true; do
     # Mostrar el valor aleatorio
     echo "Valor aleatorio (entre 0 y 1): $VALOR"
     echo "Valor aleatorio (entre 0 y 1): $VALOR1"
-    
+    # Esperar entrada del usuario o tiempo
+    read -t $INTERVALO -n 1 INPUT
+    # Ejecutar la llamada al comando mosquitto_pub
+    mosquitto_pub -h 127.0.0.1 -p 5600 -t "casa/luces/dormitorio" -m "{ deviceId:'5ee9df8-ff5fd3', eventTime:'2024-06-12 dfdsf', value:$VALOR, accuracy:$FLOAT_NUM }"
     # Si el usuario presiona 'q', salir del bucle
     if [ "$INPUT" == "q" ]; then
         echo "Saliendo del script..."
